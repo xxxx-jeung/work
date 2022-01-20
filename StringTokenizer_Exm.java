6aa7a0940ea4741c3d97074793d3321c9a915d72
xxxx-jeung <63729676+xxxx-jeung@users.noreply.github.com>
2022-01-20 09:40:10 +0900

    Create StringTokenizer_Exm.java
    버퍼 읽기와 버퍼 쓰기 기능에 대한 분석.

---
+++ StringTokenizer_Exm.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            String s = br.readLine();
+            StringTokenizer stk = new StringTokenizer(s);
+            int A = Integer.parseInt(stk.nextToken());
+            int B = Integer.parseInt(stk.nextToken());
+
+            bw.write(String.valueOf((A + B)));
+            bw.newLine();
+
+        }
+        bw.flush();
+        bw.close();
+    }
+}
